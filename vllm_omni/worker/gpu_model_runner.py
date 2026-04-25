6365d67f84a66963c2931ef05bdc55aac3aa91c0
Viacheslav Klimkov <vklimkov@nvidia.com>
2026-04-25 14:30:14 +0200

    Initial commit: qwen3tts NV version
    Signed-off-by: Viacheslav Klimkov <vklimkov@nvidia.com>

--- vllm_omni/worker/gpu_model_runner.py
+++ vllm_omni/worker/gpu_model_runner.py
@@ -1292,7 +1292,11 @@ class OmniGPUModelRunner(GPUModelRunner):
                 req_infos["request_id"] = req_id
                 embed_slice = inputs_embeds[s:e] if inputs_embeds is not None else None
                 req_input_ids, req_embeds, update_dict = self.model.preprocess(
-                    input_ids=input_ids[s:e], input_embeds=embed_slice, **req_infos
+                    input_ids=input_ids[s:e],
+                    input_embeds=embed_slice,
+                    start=s,
+                    end=e,
+                    **req_infos,
                 )
                 if inputs_embeds is None:
                     inputs_embeds = torch.empty(
