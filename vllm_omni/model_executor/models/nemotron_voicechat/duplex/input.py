6ea2c3b795acde3779b15453b9dd0b3709e0a517
Sy03 <1370724210@qq.com>
2026-09-23 11:59:29 +0800

    refactor(nemotron): reuse ordered PCM reservation helpers
    Signed-off-by: Sy03 <1370724210@qq.com>

--- vllm_omni/model_executor/models/nemotron_voicechat/duplex/input.py
+++ vllm_omni/model_executor/models/nemotron_voicechat/duplex/input.py
@@ -10,6 +10,10 @@ import binascii
 import numpy as np
 import pybase64 as base64
 
+from vllm_omni.engine.duplex.pcm_reservation import (
+    commit_ordered_reservation,
+    rollback_ordered_reservation,
+)
 from vllm_omni.engine.duplex.plugin import PcmAppendBuffer, PcmAppendReservation
 
 NEMOTRON_VOICECHAT_SAMPLE_RATE = 16000
@@ -74,7 +78,7 @@ class NemotronVoiceChatPcmAppendReservation(PcmAppendReservation):
         self._owner = owner
         self.operation_id = operation_id
         self.payload = payload
-        self.raw = raw
+        self._raw = raw
         self._active = True
 
     @property
@@ -83,30 +87,13 @@ class NemotronVoiceChatPcmAppendReservation(PcmAppendReservation):
 
     @property
     def byte_count(self) -> int:
-        return len(self.raw)
+        return len(self._raw)
 
     def commit(self) -> None:
-        if not self._active:
-            return
-        self._active = False
-        if self in self._owner._reservations:
-            self._owner._reservations.remove(self)
+        commit_ordered_reservation(self._owner._reservations, self, head_only=False)
 
     def rollback(self) -> None:
-        if not self._active:
-            return
-        try:
-            index = self._owner._reservations.index(self)
-        except ValueError:
-            self._active = False
-            return
-        restore = bytearray()
-        for reservation in self._owner._reservations[index:]:
-            if reservation._active:
-                restore.extend(reservation.raw)
-                reservation._active = False
-        del self._owner._reservations[index:]
-        self._owner._buffer[:0] = restore
+        rollback_ordered_reservation(self._owner._reservations, self, self._owner._buffer, active_only=True)
 
 
 class NemotronVoiceChatPcmAppendBuffer(PcmAppendBuffer):
